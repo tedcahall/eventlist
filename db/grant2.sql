8c8763a9a6c91829d17f509a7940556c93bd6133
Ted Cahall <cahall@cahall.com>
2022-06-26 16:17:19 -0400

    Delete grant2.sql

--- db/grant2.sql
+++
@@ -1,6 +0,0 @@
-# grant access to the MySQL user cahall
-# These used to say 'localhost' but that did not work for AWS.  Likely needed for home and % needed for AWS
-#
-CREATE DATABASE IF NOT EXISTS eventlist;
-CREATE USER 'cahall'@'%' IDENTIFIED BY 'bench315';
-GRANT ALL on eventlist.* to 'cahall'@'%';
